e5f329278a809c36c06f371cbe20f398f523c79b
Kentaro Iizuka <iizuka.kentaro@gmail.com>
2018-11-19 17:38:14 +0900

    improve user interface

--- oisubset/main.py
+++ oisubset/main.py
@@ -2,6 +2,7 @@ import argparse
 import yaml
 import os
 import shutil
+import sys
 import pandas as pd
 
 OUTPUT_DESCRIPTION_FILE_NAME = "class-descriptions.csv"
@@ -10,20 +11,29 @@ OUTPUT_ANNOTATION_FILE_NAME = "annotations-bbox.csv"
 
 def main(config):
     # Create Output Directory
-    os.mkdir(config["output_dir"])
-    os.mkdir(config["output_dir"] + "images")
+    if os.path.exists(config["output_dir"]):
+        print("OUTPUT DIRECTORY ALREADY EXIST!!")
+        remove_flag = input("Remove? Y/n: ")
+        if remove_flag == "Y":
+            shutil.rmtree(config["output_dir"])
+        else:
+            print("bye.")
+            sys.exit()
+    os.makedirs(config["output_dir"] + "images")
 
+    # Read and write Class Description file
     class_df = pd.read_csv(config["class_description_file"], header=None, names=('LabelName', "ClassName"))
     class_df = class_df[class_df["ClassName"].isin(config["target_classes"])]
     class_df.to_csv(config["output_dir"] + OUTPUT_DESCRIPTION_FILE_NAME, index=False, header=None)
 
+    # Read bbox annotation file
     bbox_df = pd.read_csv(config["annotation_bbox_file"])
 
     # Copy image files to output Directory
     result_bbox_df = pd.DataFrame(index=[], columns=bbox_df.columns)
     for label in class_df["LabelName"]:
         image_count = 0
-        print("Current Target label: ", class_df[class_df["LabelName"] == label]["ClassName"])
+        print("Current Target label: ", class_df[class_df["LabelName"] == label].values)
 
         target_bbox_df = bbox_df[bbox_df["LabelName"] == label]
         target_bbox_df.reset_index(drop=True)
@@ -35,13 +45,15 @@ def main(config):
                 break
 
             if image_count % 1000 == 0:
-                print("Copy count:", image_count)
+                print("Copy file count:", image_count)
                 print("Now copying ImageID:", image_name)
 
-            shutil.copy(config["image_dir"] + image_name + ".jpg", config["output_dir"] + "images/" + image_name + ".jpg")
+            shutil.copy(config["image_dir"] + image_name + ".jpg",
+                        config["output_dir"] + "images/" + image_name + ".jpg")
             image_count += 1
 
     result_bbox_df.to_csv(config["output_dir"] + OUTPUT_ANNOTATION_FILE_NAME, index=False)
+    print("Data Set Conversion is Succeed!: ", config["output_dir"])
 
 
 if __name__ == "__main__":
